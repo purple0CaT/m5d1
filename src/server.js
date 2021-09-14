b53ca5026b07eefad77b61e4538ba484525e9ba4
Istvan <steven.ford.j@gmail.com>
2021-09-14 14:45:42 +0300

    req

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import booksRouter from "./services/books/index.js";
 import studentsRouter from "./services/students/index.js";
 import listEndpoints from "express-list-endpoints";
@@ -8,7 +10,8 @@ const server = express();
 const port = 3003;
 // === COnfiguration | Before endpoints! ===
 // body converter
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 // ==== ROUTES / ENDPOINTS ====
 server.use("/students", studentsRouter);
 server.use("/books", booksRouter);
